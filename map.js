6a421484cc2a32a2262fb361a9c49ed63ada0cad
@monika.sw <monika.sw.ire@gmail.com>
2023-05-16 06:49:07 +0000

    map.js

--- map.js
+++ map.js
@@ -5,13 +5,24 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+}
+console.log(results);
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) { return num * 2 });
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
 
